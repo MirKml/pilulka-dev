67a3cb6b73453defdee8f50319439e0fd5dfa797
Miroslav Kubelik <koubel@seznam.cz>
2016-09-04 14:12:05 +0200

    dbdump.sh: comment

--- dbdump.sh
+++ dbdump.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# calls the mysql dump for the database container, it's common task, so I made this shortcut
 projectDir=$(dirname "$0")
 $projectDir/manage.sh exec db mysqldump -u root -plocaldb "$@"
